83e00f2b29f20f81fdc104555ee6c6a452af8646
minjun-kor <qoalswns06@gamil.com>
2025-05-08 14:16:41 +0900

    2차시-보안: cd,pwd 함수화, main 단순화

--- main.c
+++ main.c
@@ -4,40 +4,47 @@
 #include <unistd.h>
 #include <limits.h>
 
-void run_pwd() {
-    char cwd[1024];
-    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+
+void cd(char* path){
+    path += 3;
+    if (chdir(path) != 0) {
+        perror("cd 오류");
+    }
+}
+
+void pwd() {
+    char cwd[1000];
+    if (!(getcwd(cwd, sizeof(cwd)))) {
         printf("%s\n", cwd);
-    } else {
+    } 
+    else {
         perror("pwd 오류");
     }
 }
 
 int main() {
-    char command[256];
+    char command[1000];
 
     while (1) {
-        char cwd[1024];
+        char cwd[1000];
         getcwd(cwd, sizeof(cwd));
         printf("%s$ ", cwd);
 
         fgets(command, sizeof(command), stdin);
         command[strcspn(command, "\n")] = 0;
 
-        if (strcmp(command, "exit") == 0) break;
-
-        if (strncmp(command, "cd ", 3) == 0) {
-            char *path = command + 3;
-            if (chdir(path) != 0) perror("cd 오류");
-            continue;
+        if (strcmp(command, "exit") == 0){
+            break;
         }
-
-        if (strcmp(command, "pwd") == 0) {
-            run_pwd();
-            continue;
+        else if (strncmp(command, "cd ", 3) == 0) {
+            cd(command);
         }
-
-        printf("지원하지 않는 명령어입니다: %s\n", command);
+        else if (strcmp(command, "pwd") == 0) {
+            pwd();
+        }
+        else {
+            printf("지원하지 않는 명령어입니다: %s\n", command);
+        }
     }
 
     return 0;
