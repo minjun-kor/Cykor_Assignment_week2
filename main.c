a9ece092c9d5f2b6f8f9cbe01f32daf3b169b79e
minjun-kor <qoalswns06@gamil.com>
2025-05-09 17:12:53 +0900

    3차시 cd, pwd 외에 다른 명령어 구현 완료

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <limits.h>
-
+#include <sys/wait.h>
 
 void cd(char* path){
     path += 3;
@@ -14,11 +14,37 @@ void cd(char* path){
 
 void pwd() {
     char cwd[1000];
-    if (!(getcwd(cwd, sizeof(cwd)))) {
+    if (getcwd(cwd, sizeof(cwd))) {
         printf("%s\n", cwd);
     } 
     else {
         perror("pwd 오류");
+    }
+}
+
+void other_commands(char *command) {
+    char *args[100];
+    int i = 0;
+
+    char *token = strtok(command, " ");
+    while (token != NULL && i < 99)
+    {
+        args[i++] = token;
+        token = strtok(NULL, " ");
+    }
+    args[i] = NULL;
+
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("fork 실패");
+    } 
+    else if (pid == 0) {
+        execvp(args[0], args);
+        perror("명령 실행 실패");
+        exit(1);
+    } 
+    else {
+        waitpid(pid, NULL, 0);
     }
 }
 
@@ -29,11 +55,12 @@ int main() {
         char cwd[1000];
         getcwd(cwd, sizeof(cwd));
         printf("%s$ ", cwd);
+        fflush(stdout);
 
         fgets(command, sizeof(command), stdin);
         command[strcspn(command, "\n")] = 0;
 
-        if (strcmp(command, "exit") == 0){
+        if (strcmp(command, "exit") == 0) {
             break;
         }
         else if (strncmp(command, "cd ", 3) == 0) {
@@ -43,7 +70,7 @@ int main() {
             pwd();
         }
         else {
-            printf("지원하지 않는 명령어입니다: %s\n", command);
+            other_commands(command);
         }
     }
 
