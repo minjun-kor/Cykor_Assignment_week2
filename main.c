1032458f12340cb0c558ad974f044af583071cd5
minjun-kor <qoalswns06@gamil.com>
2025-05-10 18:01:29 +0900

    5차시: 단일 파이프 완성, 명령어 쪼개는 과정 함수화 완성

--- main.c
+++ main.c
@@ -5,6 +5,16 @@
 #include <limits.h>
 #include <sys/wait.h>
 
+void split_args(char *input, char **args) {
+    int i = 0;
+    char *token = strtok(input, " ");
+    while (token != NULL && i < 99) {
+        args[i++] = token;
+        token = strtok(NULL, " ");
+    }
+    args[i] = NULL;
+}
+
 void cd(char* path){
     path += 3;
     if (chdir(path) != 0) {
@@ -13,7 +23,7 @@ void cd(char* path){
 }
 
 void pwd() {
-    char cwd[1000];
+    char cwd[100];
     if (getcwd(cwd, sizeof(cwd))) {
         printf("%s\n", cwd);
     } 
@@ -24,15 +34,7 @@ void pwd() {
 
 void other_commands(char *command) {
     char *args[100];
-    int i = 0;
-
-    char *token = strtok(command, " ");
-    while (token != NULL && i < 99)
-    {
-        args[i++] = token;
-        token = strtok(NULL, " ");
-    }
-    args[i] = NULL;
+    split_args(command, args);
 
     pid_t pid = fork();
     if (pid < 0) {
@@ -48,7 +50,7 @@ void other_commands(char *command) {
     }
 }
 
-void pipe(char *command) {
+void one_pipe(char *command) {
     char *input1 = strtok(command, "|");
     char *input2 = strtok(NULL, "|");
 
@@ -58,30 +60,45 @@ void pipe(char *command) {
     }
 
     char *args1[100], *args2[100];
-    int i = 0;
-    char *token = strtok(input1, " ");
-    while (token != NULL) {
-        args1[i++] = token;
-        token = strtok(NULL, " ");
-    }
-    args1[i] = NULL;
+    split_args(input1, args1);
+    split_args(input2, args2);
 
-    i = 0;
-    token = strtok(input2, " ");
-    while (token != NULL) {
-        args2[i++] = token;
-        token = strtok(NULL, " ");
+    int fd[2];
+    pipe(fd);
+
+    pid_t pid1 = fork();
+    if (pid1 == 0) {
+        dup2(fd[1], STDOUT_FILENO);
+        close(fd[0]);
+        close(fd[1]);
+        execvp(args1[0], args1);
+        perror("파이프 왼쪽 실행 실패");
+        exit(1);
     }
-    args2[i] = NULL;
+
+    pid_t pid2 = fork();
+    if (pid2 == 0) {
+        dup2(fd[0], STDIN_FILENO);
+        close(fd[1]);
+        close(fd[0]);
+        execvp(args2[0], args2);
+        perror("파이프 오른쪽 실행 실패");
+        exit(1);
+    }
+
+    close(fd[0]);
+    close(fd[1]);
+    waitpid(pid1, NULL, 0);
+    waitpid(pid2, NULL, 0);
 
 }
 
 
 int main() {
-    char command[1000];
+    char command[100];
 
     while (1) {
-        char cwd[1000];
+        char cwd[100];
         getcwd(cwd, sizeof(cwd));
         printf("%s$ ", cwd);
         fflush(stdout);
@@ -99,7 +116,7 @@ int main() {
             pwd();
         }
         else if (strchr(command, '|') != NULL) {
-            pipe(command);
+            one_pipe(command);
         }
         else {
             other_commands(command);
